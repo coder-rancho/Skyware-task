4fcbeea82d607b11a19acea1849bc13a6ce8664a
coder_rancho local <test.namanvyas@gmail.com>
2021-11-17 20:35:12 +0530

    add port for deployment

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 import express from 'express'
 import users from './users.mjs'
-console.log(users)
+
 const app = express()
 
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 
-app.listen(3000)
-
-app.post('/login', (req, res) => {
-    console.log(req.body)
-    res.send('logged in')
+app.post('/login', (req, res, next) => {
+    let user = req.body.user
+    let pass = req.body.pass
+    let userDetail
+    let userFound = false
+    for (userDetail of users) {
+        if (userDetail.user == user) {
+            userFound = true
+            if (userDetail.pass == pass) {
+                res.send(`Hi ${user}, you are successfully logged in.`)
+            } else {
+                res.send(`Incorrect password`)
+            }
+        }
+    }
+    // if user not found after looping
+    if (!userFound) {
+        res.send(`${user} doesn't exist.`)
+    }
 })
+
+
+const port = process.env.PORT ||3000
+app.listen(port)
